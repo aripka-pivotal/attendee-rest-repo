2fe1d7eb75a1ea89119ea52fdb10281361358018
Andrew Ripka <aripka@pivotal.io>
2016-05-12 11:41:51 -0500

    initial checkin of start.spring.io code

---
+++ src/test/java/io/pivotal/workshop/AttendeeRestRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.workshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AttendeeRestRepoApplication.class)
+public class AttendeeRestRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
